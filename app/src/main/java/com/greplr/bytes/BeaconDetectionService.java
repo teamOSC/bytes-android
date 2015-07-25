380fb8c45be3cfebda089b269f90233508127a0c
Prempal Singh <prempal.42@gmail.com>
2015-07-25 20:19:59 +0530

    added activities

---
+++ app/src/main/java/com/greplr/bytes/BeaconDetectionService.java
@@ -0,0 +1,16 @@
+package com.greplr.bytes;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class BeaconDetectionService extends Service {
+    public BeaconDetectionService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
--- app/src/main/java/com/greplr/bytes/ResultActivity.java
+++ app/src/main/java/com/greplr/bytes/ResultActivity.java
@@ -1,22 +1,22 @@
 package com.greplr.bytes;
 
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class ResultActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_result);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_result, menu);
         return true;
     }
 

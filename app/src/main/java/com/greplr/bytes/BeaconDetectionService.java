7227bea6832bcf3999f866e1b8bf22e59324c563
Prempal Singh <prempal.42@gmail.com>
2015-07-25 23:01:50 +0530

    detecting beacon

--- app/src/main/java/com/greplr/bytes/BeaconDetectionService.java
+++ app/src/main/java/com/greplr/bytes/BeaconDetectionService.java
@@ -3,9 +3,60 @@ package com.greplr.bytes;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.os.RemoteException;
+import android.util.Log;
+
+import com.estimote.sdk.Beacon;
+import com.estimote.sdk.BeaconManager;
+import com.estimote.sdk.Region;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class BeaconDetectionService extends Service {
+
+    private BeaconManager beaconManager;
+    private com.estimote.sdk.Region region;
+
     public BeaconDetectionService() {
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return START_STICKY;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        region = new Region("regionId", "b9407f30-f5f8-466e-aff9-25556b57fe6d", 43, 22317);
+        beaconManager = new BeaconManager(this);
+
+        beaconManager.setBackgroundScanPeriod(TimeUnit.SECONDS.toMillis(1), 0);
+
+        beaconManager.setMonitoringListener(new BeaconManager.MonitoringListener() {
+            @Override
+            public void onEnteredRegion(Region region, List<Beacon> beacons) {
+                Log.d("Main", "Entered region");
+
+            }
+
+            @Override
+            public void onExitedRegion(Region region) {
+                Log.d("Main", "Exited region");
+            }
+        });
+
+        beaconManager.connect(new BeaconManager.ServiceReadyCallback() {
+            @Override
+            public void onServiceReady() {
+                try {
+                    beaconManager.startMonitoring(region);
+                } catch (RemoteException e) {
+                    Log.d("MainActivity", "Error while starting monitoring");
+                }
+            }
+        });
     }
 
     @Override
--- app/src/main/java/com/greplr/bytes/MainActivity.java
+++ app/src/main/java/com/greplr/bytes/MainActivity.java
@@ -1,5 +1,6 @@
 package com.greplr.bytes;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
@@ -11,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        startService(new Intent(this, BeaconDetectionService.class));
     }
 
     @Override
